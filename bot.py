12c52c0b173ea81fab34b32633c2f75a2df492fa
Sayhan Rahman <sayhanrahman@gokkusagi.k12.tr>
2024-01-24 14:26:43 +0300

    Major changes

---
+++ bot.py
@@ -0,0 +1,25 @@
+# bot.py
+
+from flask import Flask, render_template
+import subprocess
+from threading import Thread
+
+
+# Flask setup
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Function to start JMusicBot
+def start_jmusicbot():
+    subprocess.run(['java', '-Dnogui=true', '-jar', 'JMusicBot-X.Y.Z.jar'])
+
+# Run Flask and Discord bot
+if __name__ == "__main__":
+    # Start JMusicBot in a separate thread
+    Thread(target=start_jmusicbot).start()
+
+    # Start Flask in the main thread
+    app.run(host='0.0.0.0', port=8080)
